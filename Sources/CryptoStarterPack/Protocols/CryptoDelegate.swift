16d5ce72fda56fa2bd0d67c186cee90e600c4eaa
Pumper Knickle <pumperknickle.protonmail.com>
2019-09-16 07:22:54 -0700

    refactor hashing interface

--- Sources/CryptoStarterPack/Protocols/CryptoDelegate.swift
+++ Sources/CryptoStarterPack/Protocols/CryptoDelegate.swift
@@ -3,9 +3,3 @@ import Foundation
 public protocol CryptoDelegate {
     static func hash(_ input: [Bool]) -> [Bool]?
 }
-
-public extension CryptoDelegate {
-    static func hash(_ input: Data) -> [Bool]? {
-        return Self.hash(input.toBoolArray())
-    }
-}
--- Tests/CryptoStarterPackTests/BaseCryptoSpec.swift
+++ Tests/CryptoStarterPackTests/BaseCryptoSpec.swift
@@ -7,8 +7,7 @@ final class BaseCryptoSpec: QuickSpec {
     override func spec() {
         describe("The SHA 256 hashing algorithm") {
             it("hashes a message into a 256 bit output") {
-                expect(BaseCrypto.hash("The yellow fox crossed the road.".data(using: .utf8)!)).toNot(beNil())
-                expect(BaseCrypto.hash("The yellow fox crossed the road.".data(using: .utf8)!.toBoolArray())).toNot(beNil())
+                expect(BaseCrypto.hash("The yellow fox crossed the road.".toBoolArray())).toNot(beNil())
             }
         }
     }
