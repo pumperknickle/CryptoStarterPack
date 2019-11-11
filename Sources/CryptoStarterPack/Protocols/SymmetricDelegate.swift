d02cea659bc87617fda8b938e656eb9619820b6f
Pumper Knickle <pumperknickle.protonmail.com>
2019-11-11 07:08:35 -0800

    IV is fixed

--- Sources/CryptoStarterPack/Protocols/SymmetricDelegate.swift
+++ Sources/CryptoStarterPack/Protocols/SymmetricDelegate.swift
@@ -3,7 +3,7 @@ import Bedrock
 
 public protocol SymmetricDelegate {
     associatedtype Key: Stringable, Randomizable, DataEncodable
-	associatedtype IV: DataEncodable, Randomizable
+	associatedtype IV: DataEncodable, Randomizable, Fixed
 	static func encrypt(plainText: [Bool], key: Key, iv: IV?) -> [Bool]?
 	static func decrypt(cipherText: [Bool], key: Key, iv: IV?) -> [Bool]?
 }
