fedee481393de473aab8246364b24b4fb8d98452
Pumper Knickle <pumperknickle.protonmail.com>
2021-04-07 10:17:43 -0500

    upgrade crypto swift

--- Package.swift
+++ Package.swift
@@ -1,9 +1,15 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.2
 
 import PackageDescription
 
 let package = Package(
     name: "CryptoStarterPack",
+    platforms: [
+        .iOS(.v12),
+        .tvOS(.v12),
+        .watchOS(.v5),
+        .macOS(.v10_15)
+    ],
     products: [
         .library(
             name: "CryptoStarterPack",
@@ -11,10 +17,10 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.2.0"),
-        .package(url: "https://github.com/pumperknickle/Bedrock.git", from: "0.1.9"),
-        .package(url: "https://github.com/vapor/crypto.git", from: "3.3.2"),
-        .package(url: "https://github.com/Quick/Quick.git", from: "2.1.0"),
-        .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.2"),
+        .package(url: "https://github.com/pumperknickle/Bedrock.git", from: "0.2.1"),
+        .package(name: "Crypto", url: "https://github.com/vapor/crypto.git", from: "3.3.2"),
+        .package(url: "https://github.com/Quick/Quick.git", from: "3.1.2"),
+        .package(url: "https://github.com/Quick/Nimble.git", from: "9.0.0"),
     ],
     targets: [
         .target(
